6ce9c79397a6faee3f0cee1b38965669649b0bde
pcodesdev <njugunapm23@gmail.com>
2024-02-03 23:25:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Accordion from './components/Accordion'
+
+function App() {
+
+  return (
+    <>
+      <Accordion/>
+    </>
+  )
+}
+
+export default App
+
+// TODO: Making a reusable button
